edba51e1f9646ee3fb1794c3b8b55e7c12f4d69d
wilwe <wilwe036@gmail.com>
2023-12-16 20:01:49 +0100

    Update menu.js

--- config/ags/js/widget/menu.js
+++ config/ags/js/widget/menu.js
@@ -1,14 +1,14 @@
 import Widget from 'resource:///com/github/Aylur/ags/widget.js';
 import icons from '../icons.js';
-import PowerMenu from '../services/powermenu.js';
 import options from '../options.js';
+import * as Utils from 'resource:///com/github/Aylur/ags/utils.js';
 
 /**
  * @param {'sleep' | 'reboot' | 'logout' | 'shutdown'} action
  * @param {string} label
  */
 const SysButton = (action, icon) => Widget.Button({
-    on_clicked: () => PowerMenu(action),
+    on_clicked: () => Utils.exec(action),
     child: Widget.Box({
         vertical: true,
         children: [
@@ -22,23 +22,24 @@ const visibility = self => {
 };
 
 export default monitor => Widget.Window({
-    //connections: [['draw', visibility], ...conn],
     binds: [['visible', options.menu.visible]],
     name: 'menu',
     expand: true,
     monitor,
+    layer: 'background',
+    exclusivity: 'ignore',
     child: Widget.Box({
         vertical: true,
         children: [
             Widget.Box({
                 children: [
-                SysButton('librewolf', 'io.gitlab.librewolf-community'),
-                SysButton('reboot', icons.powermenu.reboot)
+                SysButton('flatpak run io.gitlab.librewolf-community --new-window --profile /home/wilwe/.var/app/io.gitlab.librewolf-community/.librewolf/h9joi49y.default-default', 'io.gitlab.librewolf-community'),
+                SysButton('kitty', 'kitty')
             ]}),
             Widget.Box({
                 children: [
-                SysButton('sleep', icons.powermenu.sleep),
-                SysButton('logout', icons.powermenu.logout),
+                SysButton('/usr/bin/flatpak run --branch=stable --arch=x86_64 --command=/app/bin/steam --file-forwarding com.valvesoftware.Steam @@u %U @@', 'com.valvesoftware.Steam'),
+                SysButton('/usr/bin/flatpak run --branch=stable --arch=x86_64 --command=minecraft com.mojang.Minecraft', 'com.mojang.Minecraft'),
             ]}),
         ],
     }),
