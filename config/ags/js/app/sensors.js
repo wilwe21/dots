59ed065768e6fae37400eda9a6f9427a50946fd8
wilwe <wilwe036@gmail.com>
2024-05-06 15:02:29 +0200

    sensors for multi monitors

--- config/ags/js/app/sensors.js
+++ config/ags/js/app/sensors.js
@@ -4,18 +4,21 @@ import Applications from 'resource:///com/github/Aylur/ags/service/applications.
 import FontIcon from '../misc/FontIcon.js';
 import icons from '../icons.js';
 import * as Utils from 'resource:///com/github/Aylur/ags/utils.js';
+import WW from '../misc/WW.js'
 import options from '../options.js';
 
-export default sensors => Widget.Window({
-    name: 'sensors',
+export default () => WW({
+    name: `sensors`,
     layer: "overlay",
-    class_name: 'senswin',
     keymode: 'none',
     anchor: ['top', 'right'],
     visible: false,
-    child: Widget.Label({
-        class_name: 'sensorslab',
-    }).poll(1000, self => {
-	self.label = Utils.exec("sensors -A")
-    })
+	child: Widget.Box({
+		class_name: 'senswin',
+		child: Widget.Label({
+			class_name: 'sensorslab',
+		}).poll(1000, self => {
+		self.label = Utils.exec("sensors -A")
+		})
+	})
 });
