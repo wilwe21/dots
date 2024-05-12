4d250e4a73b25a34a12609b84d222a5832a50f14
wilwe <wilwe036@gmail.com>
2024-05-12 20:51:21 +0200

    black hole update

--- shaders/test.glsl
+++ shaders/test.glsl
@@ -5,7 +5,7 @@ uniform sampler2D tex;
 void main() {
 	float angle = 6.28318;
 	float distance = distance(v_texcoord, vec2(0.5, 0.5));
-	float warpFactor = smoothstep(1.0, 0.25, distance);
+	float warpFactor = smoothstep(0.0, 0.25, distance);
 	vec2 rotatedTexCoord = v_texcoord + warpFactor * (vec2(sin(angle), cos(angle)));
 	rotatedTexCoord = mod(rotatedTexCoord, vec2(1.0));
 	vec4 pixColor = texture2D(tex, rotatedTexCoord);
