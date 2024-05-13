fdb9fcecb1ffdb21093dab69cc3d6f5bc4e8d39d
wilwe <wilwe036@gmail.com>
2024-05-13 18:48:53 +0200

    fuck

--- shaders/test.glsl
+++ shaders/test.glsl
@@ -3,11 +3,17 @@ varying vec2 v_texcoord;
 uniform sampler2D tex;
 
 void main() {
-	float angle = 6.28318;
-	float distance = distance(v_texcoord, vec2(0.5, 0.5));
-	float warpFactor = smoothstep(0.0, 0.25, distance);
-	vec2 rotatedTexCoord = v_texcoord + warpFactor * (vec2(sin(angle), cos(angle)));
-	rotatedTexCoord = mod(rotatedTexCoord, vec2(1.0));
-	vec4 pixColor = texture2D(tex, rotatedTexCoord);
-	gl_FragColor = pixColor;
+	float distanceToCenter = distance(v_texcoord, vec2(0.5, 0.5));
+	float curvature = pow(distanceToCenter, 12.0);
+
+	vec2 distortedTexCoord = v_texcoord + curvature * (v_texcoord - vec2(0.5, 0.5));
+
+	float scanlineIntensity = sin(v_texcoord.y * 20.0) * 0.2;
+
+	float fadeFactor = smoothstep(1.0, 0.2, distanceToCenter);
+
+	vec3 noise = vec3(fract(sin(v_texcoord.x * 100.0) * 1000.0));
+
+	vec4 pixColor = texture2D(tex, distortedTexCoord);
+	gl_FragColor = (pixColor * fadeFactor * (1.0 - scanlineIntensity)) + noise;
 }
