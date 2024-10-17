b006c45b24441b6845457a1d4b2de04830a2f2c1
wilwe <wilwe036@gmail.com>
2024-10-17 20:06:35 +0200

    js is wierd language

--- config/ags/js/misc/mpris.js
+++ config/ags/js/misc/mpris.js
@@ -4,11 +4,14 @@ import icons from '../icons.js';
 import { blurImg } from '../utils.js';
 
 function rep(n, s, e) {
-	if (n.toLowerCase().search(`${s}`) > -1 && n.toLowerCase().search(`${e}`) > -1) {
-		var naw = n.substring(n.toLowerCase().search(`${s}`)+1, n.toLowerCase().search(`${e}`)).toLowerCase()
-		if (naw.search("cover") > -1 || naw.search("animatic") > -1 || naw.search("animation") > -1 || naw.search("lyrics") > -1 || naw.search("remix") > -1 || naw.search("video") > -1 || naw.search("remastered") > -1 || naw.search("video") > -1 || naw.search("music") > -1 || naw.search("read") > -1 || naw.search("description") > -1) {
-			var reg = new RegExp(`${s}`+naw+`${e}`, "gi");
-			var n = n.replace(reg, "")
+	let arr = [...n]
+	for (let i = 0; i < arr.filter( x => x === `${s}`).length + 1; i++) {
+		if (n.toLowerCase().search(`${s}`) > -1 && n.toLowerCase().search(`${e}`) > -1) {
+			var naw = n.substring(n.toLowerCase().search(`${s}`)+1, n.toLowerCase().search(`${e}`)).toLowerCase()
+			if (naw.search("cover") > -1 || naw.search("animatic") > -1 || naw.search("animation") > -1 || naw.search("lyrics") > -1 || naw.search("remix") > -1 || naw.search("video") > -1 || naw.search("remastered") > -1 || naw.search("video") > -1 || naw.search("music") > -1 || naw.search("read") > -1 || naw.search("description") > -1 || naw.search("fan") > -1 || naw.search("song") > -1) {
+				var reg = new RegExp(`${s}`+naw+`${e}`, "gi");
+				n = n.replace(reg, "")
+			}
 		}
 	}
 	return n
