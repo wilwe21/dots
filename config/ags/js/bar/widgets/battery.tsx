4529d46de06629419cd3e77a8d45e76b16f18e62
wilwe <wilwe036@gmail.com>
2025-01-11 15:21:15 +0100

    battery bind

--- config/ags/js/bar/widgets/battery.tsx
+++ config/ags/js/bar/widgets/battery.tsx
@@ -1,9 +1,9 @@
+import { bind } from "astal"
 import { Gtk } from "astal/gtk3"
 import Battery from "gi://AstalBattery"
 
 const bat = Battery.get_default()
 
 export default function BatteryBox() {
-	let proc = String(parseInt(bat.percentage*100))
-	return <label label={proc+"%"} />
+	return <box>{bind(bat, "percentage").as(v=> <label label={`${parseInt(v*100)}%`} />)}</box>
 }
